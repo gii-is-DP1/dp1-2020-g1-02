ef477c35e64fdf9e1922fe39162ec70ab2be9921
Pablo <apblogonmon@gmail.com>
2020-11-08 17:53:15 +0100

    event

---
+++ src/main/java/org/springframework/samples/petclinic/service/EventService.java
@@ -0,0 +1,17 @@
+package org.springframework.samples.petclinic.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.repository.EventRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class EventService {
+	@Autowired
+	private EventRepository eventRepo;
+	
+	@Transactional
+	public int eventCount() {
+		return (int) eventRepo.count();
+	}
+}
