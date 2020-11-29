a2488e893ddfe053c59b6abaa35d799d4f4a80de
Pablo <apblogonmon@gmail.com>
2020-11-29 14:17:02 +0100

    tests Proveedor

--- src/test/java/org/springframework/samples/petclinic/service/FacturaServiceTest.java
+++ src/test/java/org/springframework/samples/petclinic/service/FacturaServiceTest.java
@@ -1,20 +1,22 @@
 package org.springframework.samples.petclinic.service;
 
-import static org.junit.Assert.assertNull;
+
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
+import java.time.LocalDate;
+
 import java.util.Iterator;
-import java.util.Random;
+
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.samples.petclinic.model.Factura;
-import org.springframework.samples.petclinic.repository.FacturaRepository;
 import org.springframework.stereotype.Service;
+
 
 @DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
 public class FacturaServiceTest {
@@ -33,6 +35,37 @@ public class FacturaServiceTest {
 		Factura facturaFind = facturaService.findFacturaById(1).get();
 		assertEquals(Factura.class, facturaFind.getClass());
 	}
+	
+//	@Test
+//	public void testSaveFactura() {
+//		Integer cantidadFacturasIni = 0;
+//		Iterable<Factura> facturaFind = facturaService.findAll();
+//		Iterator<Factura> iterador = facturaFind.iterator();
+//		while(iterador.hasNext()) cantidadFacturasIni++;
+//		
+//		Factura facturaNew = new Factura();
+//		facturaNew.setId(10);
+//		facturaNew.setId_ped(3);
+//		facturaNew.setId_prov(1);
+//		facturaNew.setPrecio_total(50.0);
+//		facturaNew.setFecha(LocalDate.now());
+//		facturaService.save(facturaNew);
+//		
+//		Integer cantidadFacturasFin = 0;
+//		Iterable<Factura> facturaFind2 = facturaService.findAll();
+//		Iterator<Factura> iterador2 = facturaFind2.iterator();
+//		while(iterador2.hasNext()) cantidadFacturasFin++;
+//		
+//		assertEquals(cantidadFacturasIni+1, cantidadFacturasFin);
+//		
+//		
+//		Factura facturaEdit = facturaService.findFacturaById(1).get();
+//		facturaEdit.setPrecio_total(3.5);
+//		facturaEdit.setId_ped(100);;
+//		facturaService.save(facturaEdit);
+//		assertEquals(3.5, facturaService.findFacturaById(1).get().getPrecio_total());
+//	}
+	
 	
 	@Test
 	public void testFindAllFacturasByProveedorId() {
