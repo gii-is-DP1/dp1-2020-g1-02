c59ca0e71b20ee5308b7b77b44d5fe180733f3ef
Pablo <apblogonmon@gmail.com>
2021-01-11 18:19:42 +0100

    Iniciando con tests de controladores y tests con mocks

--- src/main/java/org/springframework/samples/petclinic/model/Oferta.java
+++ src/main/java/org/springframework/samples/petclinic/model/Oferta.java
@@ -4,6 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 
@@ -22,4 +23,7 @@ public class Oferta extends NamedEntity {
     @JoinColumn(name="producto_id")
     private Producto producto;
 	
+//	@OneToOne(optional=false)
+//	@JoinColumn(name="proveedor")
+//	private Proveedor proveedor;
 }
