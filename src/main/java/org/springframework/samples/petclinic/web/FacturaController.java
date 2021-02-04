b6f30f14db2f0701ae21be9fa1adb3daf660f08a
José Carlos Romero Pozo <josrompoz1@alum.us.es>
2021-02-04 12:45:00 +0100

    Arreglo cosas

--- src/main/java/org/springframework/samples/petclinic/web/FacturaController.java
+++ src/main/java/org/springframework/samples/petclinic/web/FacturaController.java
@@ -1,27 +1,16 @@
 package org.springframework.samples.petclinic.web;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
-
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.samples.petclinic.model.Cliente;
 import org.springframework.samples.petclinic.model.Factura;
 import org.springframework.samples.petclinic.model.Proveedor;
-import org.springframework.samples.petclinic.model.Servicio;
 import org.springframework.samples.petclinic.service.FacturaService;
 import org.springframework.samples.petclinic.service.ProveedorService;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -49,8 +38,6 @@ public class FacturaController {
 		modelMap.addAttribute("factura", factura);
 		return vista;
 	}
-	
-	
 	
 	@GetMapping(path="/filtrado")
 	public String filtradoFactura(String nameProv,ModelMap modelMap) {
